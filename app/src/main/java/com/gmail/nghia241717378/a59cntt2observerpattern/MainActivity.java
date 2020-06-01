e9a43e186c3a15d203e802381ce3851b56503891
meocutee123 <49399040+meocutee123@users.noreply.github.com>
2020-06-01 19:37:40 +0700

    chep theo mai ong chui t deo biet dau nhe!

---
+++ app/src/main/java/com/gmail/nghia241717378/a59cntt2observerpattern/MainActivity.java
@@ -0,0 +1,78 @@
+package com.gmail.nghia241717378.a59cntt2observerpattern;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TimePicker;
+
+import com.gmail.nghia241717378.a59cntt2observerpattern.Untilites.MyDialog;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements MyDialog.onMyDateChangeListener {
+     EditText edtNgayDi, edtGioDi;
+     ImageView imgDatePicker, imgTimePicker;
+     Context context = this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addEvents();
+        addViews();
+    }
+
+    private void addEvents() {
+        edtNgayDi = findViewById(R.id.edtNgayDi);
+        edtNgayDi.setEnabled(false);
+        edtGioDi = findViewById(R.id.edtGioDi);
+        edtGioDi.setEnabled(false);
+        imgDatePicker = findViewById(R.id.imgDatePicker);
+        imgTimePicker = findViewById(R.id.ImgTimePicker);
+
+
+
+    }
+
+    private void addViews() {
+        imgDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyDialog myDialog = new MyDialog(Calendar.getInstance(), MainActivity.this, MainActivity.this);
+                myDialog.showDialog();
+
+            }
+        });
+        imgTimePicker.setOnClickListener(new View.OnClickListener() {
+            Calendar calendar = Calendar.getInstance();
+            final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+            final int minute = calendar.get(Calendar.MINUTE);
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(context, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        edtGioDi.setText(hourOfDay + ":" + minute);
+                    }
+                }, hour, minute, DateFormat.is24HourFormat(context));
+                timePickerDialog.show();
+
+            }
+        });
+    }
+
+    @Override
+    public void updateDate(Calendar newDate) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(newDate.get(Calendar.DAY_OF_MONTH)).append("/")
+                .append(newDate.get(Calendar.MONTH) + 1).append("/")
+                .append(newDate.get(Calendar.YEAR));
+        edtNgayDi.setText(stringBuilder.toString());
+    }
+
+}
---
+++ app/src/main/java/com/gmail/nghia241717378/a59cntt2observerpattern/Untilites/MyDialog.java
@@ -0,0 +1,41 @@
+package com.gmail.nghia241717378.a59cntt2observerpattern.Untilites;
+
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+public class MyDialog {
+    private Calendar calendar;
+    private onMyDateChangeListener onMyDateChangeListener;
+    private Context context;
+
+    public MyDialog(Calendar calendar, MyDialog.onMyDateChangeListener onMyDateChangeListener, Context context) {
+        this.calendar = calendar;
+        this.onMyDateChangeListener = onMyDateChangeListener;
+        this.context = context;
+    }
+
+    public void showDialog(){
+        final DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener(){
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(year, month, dayOfMonth);
+                onMyDateChangeListener.updateDate(calendar);
+            }
+        };
+        DatePickerDialog datePickerDialog = new DatePickerDialog(context, onDateSetListener,
+                calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE)){
+
+        };
+        datePickerDialog.show();;
+    }
+    public interface onMyDateChangeListener{
+
+        void updateDate(Calendar newDate);
+
+
+    }
+}
